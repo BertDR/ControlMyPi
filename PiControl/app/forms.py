9a9ec20e81b693e355d09ea2ad8569f5fa8c0ecf
BertDR <45014466+BertDR@users.noreply.github.com>
2018-11-13 20:26:09 +0100

    Initial Checkin

---
+++ PiControl/app/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password')
+    password2 = PasswordField(
+        'Repeat Password', validators=[EqualTo('password')])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+class ServerInfoForm(FlaskForm):
+    PublicIP : StringField('PublicIP')
+    PrivateIP : StringField('PrivateIP')
+
---
+++ PiControl/app/routes.py
@@ -0,0 +1,68 @@
+﻿from app import app
+from flask_login import current_user, login_user, logout_user, login_required
+from app import db
+from app.forms import LoginForm, EditProfileForm, ServerInfoForm
+from app.models import User
+from flask import render_template, flash, redirect, url_for, request
+import socket
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html');
+
+@app.route('/users', methods=['GET', 'POST'])
+@login_required
+def users():
+    users = User.query.order_by(User.username).all()
+    return render_template('users.html',users=users)
+
+@app.route('/user/<id>', methods=['GET', 'POST'])
+@login_required
+def user(id):
+    user = User.query.filter_by(id=id).first_or_404()
+    form = EditProfileForm(user.username)
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.about_me = form.about_me.data
+        user.email = form.email.data
+        if form.password.data != '':
+            user.set_password(form.password.data)
+
+        db.session.commit()
+        flash ('Your changes have been saved.')
+        return redirect(url_for('user', id=user.id))
+    elif request.method == 'GET':
+        form.username.data = user.username
+        form.about_me.data = user.about_me
+        form.email.data = user.email
+    return render_template('editprofile.html', tile='Edit profile', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('índex'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/ServerInfo', methods=['GET'])
+def serverinfo():
+    serverinfoform = ServerInfoForm()
+    hostname = socket.gethostname()
+    import ipgetter
+    serverinfoform.PublicIP = ipgetter.myip()
+    serverinfoform.PrivateIP = socket.gethostbyname(hostname);
+    return render_template('serverinfo.html',form=serverinfoform)
